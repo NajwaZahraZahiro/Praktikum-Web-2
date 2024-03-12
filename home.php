8cfceed4f3804fa30b05eff054d871ad5a0f7e7c
NajwaZahraZahiro <146900730+NajwaZahraZahiro@users.noreply.github.com>
2024-03-12 23:16:09 +0700

    Tugas Praktikum 03

---
+++ home.php
@@ -0,0 +1,21 @@
+<!-- Task 1
+Pisah kode di bawah ke dalam file-file yang sudah dibuat sebelumnya. -->
+
+<?php
+    include_once 'header.php'
+?>
+				<h1>Welcome Home!</h1>
+				<p>
+				    <!--
+				        Tuliskan Nama - NIM - Rombel 
+				    -->
+					Nama    : Najwa Zahra Zahiro
+                    NIM     : 0110123145
+                    Rombel  : SI07
+
+                    
+				</p>
+				
+<?php
+    include_once 'footer.php'
+?>
